62788e53a01fc65546fe1d71cb9d502d44303b07
maxitect <maxime.downe@gmail.com>
2025-05-27 17:54:29 +0100

    chore: update project description in pyproject.toml

--- src/graph.py
+++ src/graph.py
@@ -4,9 +4,6 @@ from models import State
 from nodes import chatbot
 
 graph_builder = StateGraph(State)
-
-# The first argument is the unique node name
-# The second argument is the function or object that will be called whenever the node is used.
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
 
