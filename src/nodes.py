05416b2fb1789d147c46e9e07f92e1454e9b3f56
maxitect <maxime.downe@gmail.com>
2025-05-27 18:19:47 +0100

    feat: invoke llm with tools in nodes.py

--- src/nodes.py
+++ src/nodes.py
@@ -1,9 +1,11 @@
 from langchain.chat_models import init_chat_model
 
 from models import State
+from tools import tools
 
 llm = init_chat_model("openai:gpt-4.1-nano")
+llm_with_tools = llm.bind_tools(tools)
 
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
