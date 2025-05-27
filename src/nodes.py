e12f9552488f64f2e332302a2807ba2c3a2ad140
maxitect <maxime.downe@gmail.com>
2025-05-27 19:01:51 +0100

    feat: add basic tool node definition in nodes.py

--- src/nodes.py
+++ src/nodes.py
@@ -1,7 +1,41 @@
+import json
 from langchain.chat_models import init_chat_model
+from langchain_core.messages import ToolMessage
+from dotenv import load_dotenv
 
 from models import State
 from tools import tools
+
+load_dotenv()
+
+
+class BasicToolNode:
+    """A node that runs the tools requested in the last AIMessage."""
+
+    def __init__(self, tools: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
+
+
+tool_node = BasicToolNode(tools)
 
 llm = init_chat_model("openai:gpt-4.1-nano")
 llm_with_tools = llm.bind_tools(tools)
