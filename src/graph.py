2a1118e5b25962b1793e75161a1ff6a3a346ab81
maxitect <maxime.downe@gmail.com>
2025-05-27 17:37:55 +0100

    refactor: tighten comments in graph.py

--- src/graph.py
+++ src/graph.py
@@ -6,8 +6,7 @@ from nodes import chatbot
 graph_builder = StateGraph(State)
 
 # The first argument is the unique node name
-# The second argument is the function or object that will be called whenever
-# the node is used.
+# The second argument is the function or object that will be called whenever the node is used.
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
 
